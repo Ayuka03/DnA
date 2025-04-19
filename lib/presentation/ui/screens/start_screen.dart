5b602d61808bb58cc0ce8cbe61b6b9bba8d9600d
Аюка Нахныков <aukanahnykov@gmail.com>
2025-04-19 04:01:22 +0300

    2comm

--- lib/presentation/ui/screens/start_screen.dart
+++ lib/presentation/ui/screens/start_screen.dart
@@ -6,7 +6,7 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle: true, title: Text('data')),
+      appBar: AppBar(centerTitle: true),
       backgroundColor: Color.fromARGB(255, 250, 63, 40),
       body: ClipRRect(
         borderRadius: const BorderRadius.only(
