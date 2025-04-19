add8531e05834fbc1c50edd06d083031bcb86826
Даниил Бедарев <vishnyabed@gmail.com>
2025-04-19 04:20:37 +0300

    updated_main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dna_app/presentation/ui/screens/chat_list_screen.dart';
 import 'package:dna_app/presentation/ui/screens/start_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
           backgroundColor: Color.fromARGB(255, 250, 63, 40),
         ),
       ),
-      home: const StartScreen(),
+      home: const ChatListScreen(),
     );
   }
 }
---
+++ lib/presentation/ui/screens/chat_list_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ChatListScreen extends StatelessWidget {
+  const ChatListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(centerTitle: true, title: Text('data')),
+      backgroundColor: Color.fromARGB(255, 250, 63, 40),
+      body: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+        child: Scaffold(backgroundColor: Colors.white),
+      ),
+    );
+  }
+}
+ 
