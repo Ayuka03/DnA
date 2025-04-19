af083fb3bc58c50df4d0a2ba370ea09627aca3e9
Аюка Нахныков <aukanahnykov@gmail.com>
2025-04-19 03:51:47 +0300

    2commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Color.fromARGB(255, 250, 63, 40),
