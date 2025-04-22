1568cb129bff369a17df2d1c55ca12260af2ae3b
Даниил Бедарев <vishnyabed@gmail.com>
2025-04-22 21:48:18 +0300

    aaa

--- lib/presentation/ui/screens/chat_list_screen.dart
+++ lib/presentation/ui/screens/chat_list_screen.dart
@@ -14,6 +14,7 @@ class _ChatListScreenState extends State<ChatListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       appBar: AppBar(
         actions: [
           IconButton(
